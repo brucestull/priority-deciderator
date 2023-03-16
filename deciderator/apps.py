255d006e205947438f11fa79500a9babb53e30b3
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-03-16 12:37:39 -0400

    Fix typo of 'diciderator' to 'deciderator'.

--- deciderator/apps.py
+++ deciderator/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DicideratorConfig(AppConfig):
+class DecideratorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'diciderator'
+    name = 'deciderator'
